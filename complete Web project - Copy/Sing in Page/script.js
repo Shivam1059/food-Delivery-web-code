997b7dad5595771ce70680e9f950a59923cf26ec
Shivam Ahirwar <144521851+Shivama1059@users.noreply.github.com>
2023-12-22 22:41:13 +0530

    Add files via upload

---
+++ complete Web project - Copy/Sing in Page/script.js
@@ -0,0 +1,10 @@
+var x = document.getElementById("login")
+var y= document.getElementById("rigister")
+var z= document.getElementById("btn")
+
+function rigister(){
+  x.style.left="-400px"
+  y.style.left="50px"
+  z.style.left="110px"
+
+}
